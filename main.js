17f34032763b36a912251b753301c1674ef4ab2f
Farid Alfaro <farix6@gmail.com>
2023-03-21 20:31:52 -0300

    Nuevo nav

--- main.js
+++ main.js
@@ -1,47 +1,50 @@
-window.addEventListener("load", function() {
-  setTimeout(function(){
-    document.querySelector('#preloader').style.display = 'none';
-  }, 2000);
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelector("#preloader").style.display = "none";
+  }, 100);
 });
 
-const iconoMenu = document.querySelector('#icono-menu'),
-      menu = document.querySelector('#menu');
+const iconoMenu = document.querySelector("#icono-menu");
+const menu = document.querySelector("#menu");
+let isOpen = false;
 
-iconoMenu.addEventListener('click', (e) => {
-
-    // Alternamos estilos para el menú y body
-    menu.classList.toggle('active_nav');
-    document.body.classList.toggle('opacity');
-
-    // Alternamos su atributo 'src' para el ícono del menú
-    const rutaActual = e.target.getAttribute('src');
-
-    if(rutaActual == '/img/3869892-200.png'){
-        e.target.setAttribute('src','/img/open-menu2.png');
-    }else{
-        e.target.setAttribute('src','/img/3869892-200.png');
-    }
-});
-
-//nav scroll 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navPrincipal").style.top = "0";
-  } else {
-    document.getElementById("navPrincipal").style.top = "-100px";
+function cerrarMenu() {
+  if (isOpen) {
+    menu.classList.remove("active_nav");
+    document.body.classList.remove("opacity");
+    isOpen = false;
   }
-  prevScrollpos = currentScrollPos;
 }
+
+const enlacesMenu = document.querySelectorAll("#menu a");
+enlacesMenu.forEach((enlace) => {
+  enlace.addEventListener("click", (e) => {
+    cerrarMenu();
+  });
+});
+
+iconoMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Alternamos estilos para el menú y body
+  menu.classList.toggle("active_nav");
+  document.body.classList.toggle("opacity");
+  // Alternamos su atributo 'src' para el ícono del menú
+  e.target.setAttribute("src", isOpen ? "/img/3869892-200.png" : "/img/close-circle-line.png");
+  isOpen = !isOpen;
+});
+
+window.addEventListener("scroll", () => {
+  cerrarMenu();
+});
+
 
 
 
 var Textos = new Array();
 // Enter the names of the images below
-Textos[0]="Desarrollo<br>web";
-Textos[1]="Ciencia de<br>datos";
-Textos[2]="Diseño<br>multimedia";
+Textos[0] = "Desarrollo<br>web";
+Textos[1] = "Ciencia de<br>datos";
+Textos[2] = "Diseño<br>multimedia";
 
 var nuevoTexto = -1; // para empezar en el primer texto -1, con 0 comienza por mostrar el segundo
 var totalTextos = Textos.length;
@@ -52,13 +55,13 @@ function repetir() {
   if (nuevoTexto == totalTextos) {
     nuevoTexto = 0;
   }
-  var elemento = document.getElementById('text_rotate');
+  var elemento = document.getElementById("text_rotate");
   elemento.style.opacity = 0;
-  setTimeout(function() {
+  setTimeout(function () {
     elemento.innerHTML = Textos[nuevoTexto];
     elemento.style.opacity = 1;
   }, fadeDuration / 2);
-  setTimeout(repetir, 3*1000);
+  setTimeout(repetir, 3 * 1000);
 }
 
 window.onload = repetir;
