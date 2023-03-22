d56c27db5f2e6324aa9fead6b9739eb8b5e20c1a
Farid Alfaro <farix6@gmail.com>
2023-03-22 20:10:34 -0300

    Cuarto intento, correción conpilación en mobile

--- js/scroll.js
+++ js/scroll.js
@@ -1,44 +1,41 @@
-let controller = new ScrollMagic.Controller();
+document.addEventListener('DOMContentLoaded', function() {
+  let controller = new ScrollMagic.Controller();
 
-let $nav = document.querySelector("nav");
-let logo = document.getElementById("logo1");
-let ubicacionPrincipal = window.pageYOffset;
+  let $nav = document.querySelector("nav");
+  let logo = document.getElementById("logo1");
+  let ubicacionPrincipal = window.pageYOffset;
 
-let scene = new ScrollMagic.Scene({
+  let scene = new ScrollMagic.Scene({
     triggerElement: 'body',
     triggerHook: 0,
     reverse: true
-})
-.on("update", function(e) {
+  })
+  .on("update", function(e) {
     /* --donde nos encontramos actualmente */
     let desplazamientoActual = e.scrollPos;
-    let ubicacionPrincipal = window.pageYOffset;
 
     /* --condicion para ocultar o mostrar el menu */
     if (desplazamientoActual === 0) {
-        $nav.style.display = "";
-        $nav.style.backgroundColor = "transparent";
-        logo.style.filter = "brightness(100)";
+      $nav.style.backgroundColor = "transparent";
+      logo.style.filter = "brightness(100)";
     } else if (ubicacionPrincipal >= desplazamientoActual) {
-        /* --si es mayor o igual se muestra */
-        $nav.style.display = "";
-        $nav.style.backgroundColor = "#44b9e5";
-        $nav.style.top = "0px";
-        console.log('Ubicacion Principal');
-        console.log(ubicacionPrincipal);
-        console.log('desplazamiento');
-        console.log(desplazamientoActual);
+      $nav.style.backgroundColor = "#44b9e5";
+      $nav.style.top = "0px";
+      console.log('Ubicacion Principal');
+      console.log(ubicacionPrincipal);
+      console.log('desplazamiento');
+      console.log(desplazamientoActual);
     } else {
-        /* --sino lo ocultamos añadiendo un top negativo */
-        $nav.style.top = "-80px";
-        $nav.style.display = "none";
-        console.log('Ubicacion Principal');
-        console.log(ubicacionPrincipal);
-        console.log('desplazamiento');
-        console.log(desplazamientoActual);
+      $nav.style.top = "-80px";
+      $nav.style.backgroundColor = "transparent";
+      console.log('Ubicacion Principal');
+      console.log(ubicacionPrincipal);
+      console.log('desplazamiento');
+      console.log(desplazamientoActual);
     }
 
     /* --actualizamos la ubicacion principal */
     ubicacionPrincipal = desplazamientoActual;
-})
-.addTo(controller);
+  })
+  .addTo(controller);
+});
