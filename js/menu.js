e2ae531e1c7969c80eed469910d118ac61d21068
Farid Alfaro <farix6@gmail.com>
2023-03-25 08:57:38 -0300

    Nav terminado ??

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
 const shareButton = document.querySelectorAll("button.shareButton")
 
 shareButton[0].addEventListener("click", (e) => {
-    for( let i=0; i < shareButton.length; i++ ) {
-       shareButton[i].classList.toggle("open")
+    for(const element of shareButton) {
+       element.classList.toggle("open")
        shareButton[0].classList.remove("sent")
     }
 })
@@ -11,8 +11,8 @@ for( let i=1; i < shareButton.length; i++ ) {
    
    shareButton[i].addEventListener("click", (e) => {
       
-   for( let i=0; i < shareButton.length; i++ ) {
-      shareButton[i].classList.toggle("open")
+   for(const element of shareButton) {
+      element.classList.toggle("open")
    }
    shareButton[0].classList.toggle("sent")
    })
--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   let $nav = document.querySelector("nav");
   let ubicacionPrincipal = window.pageYOffset;
   let botonFlotante = document.getElementById("shareButtonJS")
+  let progreso = document.getElementById("progress_JS")
 
   let scene = new ScrollMagic.Scene({
     triggerElement: 'body',
@@ -12,12 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
   .on("update", function(e) {
     /* --donde nos encontramos actualmente */
     let desplazamientoActual = e.scrollPos;
-
     /* --condicion para ocultar o mostrar el menu */
     if (desplazamientoActual === 0) {
+      progreso.style.backgroundColor = "transparent";
       botonFlotante.style.backgroundColor = "transparent";
       $nav.style.backgroundColor = "transparent";
     } else if (ubicacionPrincipal >= desplazamientoActual) {
+      progreso.style.backgroundColor = "0A0D13";
+      botonFlotante.style.backgroundColor = "transparent";
       botonFlotante.style.backgroundColor = "";
       $nav.style.backgroundColor = "#0A0D13";
       $nav.style.top = "0px";
@@ -27,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log(desplazamientoActual);
     } else {
       botonFlotante.style.backgroundColor = "";
+      progreso.style.backgroundColor = "0A0D13";
       $nav.style.top = "-80px";
       $nav.style.backgroundColor = "transparent";
       console.log('Ubicacion Principal');
@@ -39,4 +43,20 @@ document.addEventListener('DOMContentLoaded', function() {
     ubicacionPrincipal = desplazamientoActual;
   })
   .addTo(controller);
+});
+
+const progressBarText = document.querySelectorAll('.progress-bar-text span');
+
+window.addEventListener('scroll', () => {
+  const { scrollTop } = document.documentElement;
+  
+  progressBarText.forEach((text, index) => {
+    const sectionHeight = document.querySelector(`#section-${index + 1}`).offsetHeight;
+    const sectionTop = document.querySelector(`#section-${index + 1}`).offsetTop;
+    if (scrollTop >= sectionTop && scrollTop < (sectionTop + sectionHeight)) {
+      text.classList.add('active');
+    } else {
+      text.classList.remove('active');
+    }
+  });
 });
