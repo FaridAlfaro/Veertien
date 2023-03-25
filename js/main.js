55edbce96dbcebe00f45fc98cf1d4f1770c30e9b
Farid Alfaro <farix6@gmail.com>
2023-03-25 09:13:37 -0300

    tamanio section 1

--- js/main.js
+++ js/main.js
@@ -5,22 +5,22 @@ window.addEventListener("load", function () {
 });
 
 
-var Textos = new Array();
+let Textos = new Array();
 // Enter the names of the images below
 Textos[0] = "Desarrollo<br>web";
 Textos[1] = "Ciencia de<br>datos";
 Textos[2] = "Diseño<br>multimedia";
 
-var nuevoTexto = -1; // para empezar en el primer texto -1, con 0 comienza por mostrar el segundo
-var totalTextos = Textos.length;
-var fadeDuration = 2000; // duración de la animación en milisegundos
+let nuevoTexto = -1; // para empezar en el primer texto -1, con 0 comienza por mostrar el segundo
+let totalTextos = Textos.length;
+let fadeDuration = 2000; // duración de la animación en milisegundos
 
 function repetir() {
   nuevoTexto++;
   if (nuevoTexto == totalTextos) {
     nuevoTexto = 0;
   }
-  var elemento = document.getElementById("text_rotate");
+  let elemento = document.getElementById("text_rotate");
   elemento.style.opacity = 0;
   setTimeout(function () {
     elemento.innerHTML = Textos[nuevoTexto];
@@ -32,7 +32,7 @@ function repetir() {
 window.onload = repetir;
 
 
-var colors = new Array(
+let colors = new Array(
   [62,35,255],
   [60,255,60],
   [255,35,98],
@@ -40,37 +40,37 @@ var colors = new Array(
   [255,0,255],
   [255,128,0]);
 
-var step = 0;
+let step = 0;
 //color table indices for: 
 // current color left
 // next color left
 // current color right
 // next color right
-var colorIndices = [0,1,2,3];
+let colorIndices = [0,1,2,3];
 
 //transition speed
-var gradientSpeed = 0.002;
+let gradientSpeed = 0.002;
 
 function updateGradient()
 {
   
   if ( $===undefined ) return;
   
-var c0_0 = colors[colorIndices[0]];
-var c0_1 = colors[colorIndices[1]];
-var c1_0 = colors[colorIndices[2]];
-var c1_1 = colors[colorIndices[3]];
+let c0_0 = colors[colorIndices[0]];
+let c0_1 = colors[colorIndices[1]];
+let c1_0 = colors[colorIndices[2]];
+let c1_1 = colors[colorIndices[3]];
 
-var istep = 1 - step;
-var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "rgb("+r1+","+g1+","+b1+")";
+let istep = 1 - step;
+let r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+let g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+let b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+let color1 = "rgb("+r1+","+g1+","+b1+")";
 
-var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "rgb("+r2+","+g2+","+b2+")";
+let r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+let g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+let b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+let color2 = "rgb("+r2+","+g2+","+b2+")";
 
  $('#gradient').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
