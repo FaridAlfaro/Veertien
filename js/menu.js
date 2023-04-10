7b3b7cbf44e6a9df1b29404b5d0f2fd401e96d3b
Farid Alfaro <farix6@gmail.com>
2023-04-10 01:42:59 -0300

    nuevo menu corregido

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,7 @@
-const shareButton = document.querySelectorAll("button.shareButton")
-
-shareButton[0].addEventListener("click", (e) => {
-    for(const element of shareButton) {
-       element.classList.toggle("open")
-       shareButton[0].classList.remove("sent")
-    }
-})
-
-for( let i=1; i < shareButton.length; i++ ) {
-   
-   shareButton[i].addEventListener("click", (e) => {
-      
-   for(const element of shareButton) {
-      element.classList.toggle("open")
-   }
-   shareButton[0].classList.toggle("sent")
-   })
-}
+const menuToggle = document.querySelector('[data-js="menu-toggle"]');
+$(document).ready(function(){
+  $('.menu-toggle').on('click',function(){
+    $('.menu-toggle').toggleClass('active');
+    $('.menu').toggleClass('active');
+  });
+});
