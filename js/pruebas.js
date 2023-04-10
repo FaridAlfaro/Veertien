b4b358fed87d30c16798361518de74eae1fe72e1
Farid Alfaro <farix6@gmail.com>
2023-04-10 02:33:17 -0300

    3 pasos

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,1 +1,20 @@
 
+// const shareButton = document.querySelectorAll("button.shareButton")
+
+// shareButton[0].addEventListener("click", (e) => {
+//     for(const element of shareButton) {
+//        element.classList.toggle("open")
+//        shareButton[0].classList.remove("sent")
+//     }
+// })
+
+// for( let i=1; i < shareButton.length; i++ ) {
+   
+//    shareButton[i].addEventListener("click", (e) => {
+      
+//    for(const element of shareButton) {
+//       element.classList.toggle("open")
+//    }
+//    shareButton[0].classList.toggle("sent")
+//    })
+// }
